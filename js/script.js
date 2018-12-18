4433ab0d2952dad0bd1c2f9d512acebb2aa100f9
ReuvenGam <ruvengam@users.noreply.github.com>
2018-12-18 12:16:25 +0200

    Created alert box with alert messages selected by a select dropdown

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+$(function() {
+    let lastAdded = 'all',
+        $alertBox = $('.alert-box');
+    $('select').change(function() {
+        
+        $alertBox.removeClass(lastAdded).addClass(this.value);
+        lastAdded = this.value;
+    });
+});
