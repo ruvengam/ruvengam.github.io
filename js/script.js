193556ada0fc853366337cfb0fd62647a39ac4dd
ReuvenGam <ruvengam@users.noreply.github.com>
2018-12-18 12:21:32 +0200

    Added missing files

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(function() {
     let lastAdded = 'all',
         $alertBox = $('.alert-box');
     $('select').change(function() {
-        
+
         $alertBox.removeClass(lastAdded).addClass(this.value);
         lastAdded = this.value;
     });
